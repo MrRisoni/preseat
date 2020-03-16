abf97632dc149f29a4ea58fcb2998b8de41cf670
MrRisoni <vanguelisk@gmail.com>
2020-03-16 21:11:04 +0200

    first class seats

--- src/SeatsSection.js
+++ src/SeatsSection.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SeatButton from './SeatButton';
 
-
+// https://www.seatguru.com/airlines/Lufthansa/Lufthansa_Airbus_A330-300_B.php
 class SeatsSection extends Component {
   constructor(props) {
     super(props);
@@ -15,10 +15,15 @@ class SeatsSection extends Component {
 	render()
 
 	{
-    		let sect = this.props.stmp.sections[0];
-    	console.log(sect);
+    //		let sect = this.props.stmp.sections;
+
+    let seatsHtml  = [];
+
+
+        for (let s = 0 ; s < this.props.stmp.sections.length ; s++ ) {
+   //	console.log(this.props.stmp.sections[s]);
     //	const layout = [ {pos:'W',name:'A'},{pos:'A',name:'B'},{pos:'A',name:'C'},{pos:'W',name:'D'}];
-    	const layout = [ {pos:'W',name:'A'},
+    /*	const layout = [ {pos:'W',name:'A'},
                       {pos:'C',name:'B'},
                       {pos:'A',name:'C'},
                       {pos:'A',name:'D'},
@@ -28,29 +33,43 @@ class SeatsSection extends Component {
                       {pos:'A',name:'H'},
                       {pos:'C',name:'I'},
                       {pos:'W',name:'J'}]
+                      */
+            let layout = this.props.stmp.sections[s].layout;
+            let start = this.props.stmp.sections[s].start;
+            let finish = this.props.stmp.sections[s].end;
 
+    	       for (let i =start ; i < finish; i++) {
+    		         let colsHtml = [];
+                  for (let x=0;x <layout.length ; x++) {
+      		            let el='';
+                        if (x>0) {
+                            if (layout[x].pos == 'A' && layout[x+1].pos == 'A') {
+                              el = 'ailse-left';
+                            }
+                            if (layout[x-1].pos == 'A' && layout[x].pos == 'A') {
+                              el = 'ailse-right';
+                            }
 
-    	let seatHtml  = [];
-    	for (let i =0 ; i < 12; i++) {
-    		   let colsHtml = [];
-            for (let x=0;x <layout.length ; x++) {
-      		        let el='';
-                  if (x>0) {
-                    if (layout[x].pos == 'A' && layout[x+1].pos == 'A') {
-                      el = 'ailse-left';
-                    }
-                    if (layout[x-1].pos == 'A' && layout[x].pos == 'A') {
-                      el = 'ailse-right';
-                    }
+                            if (layout[x-1].pos == 'C' && layout[x].pos == 'W') {
+                              el = 'ailse-right-big';
+                            }
+
+                          }
+
+                          if (x < layout.length -1) {
+                            if (layout[x].pos == 'W' && layout[x+1].pos == 'C') {
+                                el = 'ailse-left-big';
+                            }
+                          }
+
+    		                    colsHtml.push(<SeatButton ailseClass={el}/>);
                   }
-    		          colsHtml.push(<SeatButton ailseClass={el}/>);
-          }
-    		           seatHtml.push(<div className="row">{colsHtml}</div>);
+    		           seatsHtml.push(<div className="row">{colsHtml}</div>);
 
-      }
+           }
+        }
 
-
-    		return(<section>{seatHtml}</section>);
+    		return(<section>{seatsHtml}</section>);
 
 
     }
