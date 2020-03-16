1655c664921f2fef3e11f9c4d3288568f9bd049b
MrRisoni <vanguelisk@gmail.com>
2020-03-16 20:45:56 +0200

    dynamic css percentage

--- src/SeatsSection.js
+++ src/SeatsSection.js
@@ -17,11 +17,23 @@ class SeatsSection extends Component {
 	{
     		let sect = this.props.stmp.sections[0];
     	console.log(sect);
-    	const layout = [ {pos:'W',name:'A'},{pos:'A',name:'B'},{pos:'A',name:'C'},{pos:'W',name:'D'}];
+    //	const layout = [ {pos:'W',name:'A'},{pos:'A',name:'B'},{pos:'A',name:'C'},{pos:'W',name:'D'}];
+    	const layout = [ {pos:'W',name:'A'},
+                      {pos:'C',name:'B'},
+                      {pos:'A',name:'C'},
+                      {pos:'A',name:'D'},
+                      {pos:'C',name:'E'},
+                      {pos:'C',name:'F'},
+                      {pos:'A',name:'G'},
+                      {pos:'A',name:'H'},
+                      {pos:'C',name:'I'},
+                      {pos:'W',name:'J'}]
+
+
     	let seatHtml  = [];
     	for (let i =0 ; i < 12; i++) {
     		   let colsHtml = [];
-            for (let x=0;x <4 ; x++) {
+            for (let x=0;x <layout.length ; x++) {
       		        let el='';
                   if (x>0) {
                     if (layout[x].pos == 'A' && layout[x+1].pos == 'A') {
