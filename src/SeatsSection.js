2642a25c0c06f8fe0a6c6aeb87dd017fb99fad7a
MrRisoni <vanguelisk@gmail.com>
2020-03-22 14:23:29 +0200

    fixed row bug

--- src/SeatsSection.js
+++ src/SeatsSection.js
@@ -12,38 +12,28 @@ class SeatsSection extends Component {
 
   render() {
     let seatsHtml = [];
-
+    let el = {};
+    let btnKey = "";
     let layout = this.props.data.layout;
     let start = this.props.data.start;
     let finish = this.props.data.end;
-    console.log("layut len " + layout.length);
+
     for (let i = start; i < finish; i++) {
       let colsHtml = [];
       for (let x = 0; x < layout.length; x++) {
-        let el = "";
         let rowId = 0;
-        if (x > 0) {
-          if (layout[x].pos == "A" && layout[x + 1].pos == "A") {
-            el = "ailse-left";
-            rowId = i;
-          }
-          if (layout[x - 1].pos == "A" && layout[x].pos == "A") {
-            el = "ailse-right";
-          }
-
-          if (layout[x - 1].pos == "C" && layout[x].pos == "W") {
-            // el = "ailse-right-big";
-          }
+        if (x > 0 && layout[x].pos == "A" && layout[x + 1].pos == "A") {
+          rowId = i;
         }
 
-        if (x < layout.length - 1) {
-          if (layout[x].pos == "w" && layout[x + 1].pos == "c") {
-            //  el = "ailse-left-big";
-          }
-        }
-
+        btnKey = "stBtn_" + i + +layout[x].name;
         colsHtml.push(
-          <SeatButton colName={layout[x].name} ailseClass={el} rowId={rowId} />
+          <SeatButton
+            key={btnKey}
+            colName={layout[x].name}
+            ailseClass={el}
+            rowId={rowId}
+          />
         );
 
         if (rowId > 0) {
@@ -53,9 +43,8 @@ class SeatsSection extends Component {
             </span>
           );
         }
-
-        seatsHtml.push(<div className="row seatsHorRow">{colsHtml}</div>);
       }
+      seatsHtml.push(<div className="row seatsHorRow">{colsHtml}</div>);
     }
     return <p>{seatsHtml}</p>;
   }
