bd45adc1cdb7c581b21d742ac548c8e5ae1fe609
MrRisoni <vanguelisk@gmail.com>
2020-04-27 10:08:36 +0300

    more loops

--- src/generateMap.js
+++ src/generateMap.js
@@ -13,16 +13,36 @@ var pricingKeys = [
 
 var sections = [
   [
-    { start: 5, end: 18, layout: "WAACW", key: "OS2pha83MngY" },
-    { start: 19, end: 25, layout: "WCAACW", key: "Tz8j1EwLUM3i" }
+    { start: 5, end: 18, layoutStr: "WAACW", key: "OS2pha83MngY" },
+    { start: 19, end: 25, layoutStr: "WCAACW", key: "Tz8j1EwLUM3i" }
   ],
   [
-    { start: 9, end: 18, layout: "WCACACW", key: "Nd7IaiLHRW4G" },
-    { start: 21, end: 29, layout: "WCAACW", key: "OQjLVGnKvqfk" }
+    { start: 9, end: 18, layoutStr: "WCACACW", key: "Nd7IaiLHRW4G" },
+    { start: 21, end: 29, layoutStr: "WCAACW", key: "OQjLVGnKvqfk" }
   ],
   [
-    { start: 10, end: 18, layout: "WAACW", key: "cwpJgeTSGlCY" },
-    { start: 19, end: 25, layout: "WCAACW", key: "wvqruWMFfGBb" },
-    { start: 26, end: 38, layout: "WCAACW", key: "wTEoTjyzDiSg" }
+    { start: 10, end: 18, layoutStr: "WAACW", key: "cwpJgeTSGlCY" },
+    { start: 19, end: 25, layoutStr: "WCAACW", key: "wvqruWMFfGBb" },
+	  { start: 26, end: 38, layoutStr:: "WCAACW", key: "wTEoTjyzDiSg" }
   ]
 ];
+
+var layOutObj = [];
+for (var sg = 0; sg < segments; sg++) {
+  var subsecions = sections[s];
+  var subsectLen = subsections.length;
+
+	var sekta= [];
+for (sc =0; sc< subsectLen; sc++)
+	{
+
+		var kasta = subsecions[sc];
+
+	}
+
+
+	var newItm = {segId:sg,sections:sekta};
+
+
+
+  }
