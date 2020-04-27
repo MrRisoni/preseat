9bba2be5610969b54923df6f804ebca4d3e0dabd
MrRisoni <vanguelisk@gmail.com>
2020-04-27 08:16:56 +0300

    pricing keys

---
+++ src/generateMap.js
@@ -0,0 +1,28 @@
+var segments = 3;
+// skip 13
+var fillup = 35;
+var pricingKeys = [
+  "OS2pha83MngY",
+  "Tz8j1EwLUM3i",
+  "Nd7IaiLHRW4G",
+  "OQjLVGnKvqfk",
+  "cwpJgeTSGlCY",
+  "wvqruWMFfGBb",
+  "wTEoTjyzDiSg"
+];
+
+var sections = [
+  [
+    { start: 5, end: 18, layout: "WAACW", key: "OS2pha83MngY" },
+    { start: 19, end: 25, layout: "WCAACW", key: "Tz8j1EwLUM3i" }
+  ],
+  [
+    { start: 9, end: 18, layout: "WCACACW", key: "Nd7IaiLHRW4G" },
+    { start: 21, end: 29, layout: "WCAACW", key: "OQjLVGnKvqfk" }
+  ],
+  [
+    { start: 10, end: 18, layout: "WAACW", key: "cwpJgeTSGlCY" },
+    { start: 19, end: 25, layout: "WCAACW", key: "wvqruWMFfGBb" },
+    { start: 26, end: 38, layout: "WCAACW", key: "wTEoTjyzDiSg" }
+  ]
+];
